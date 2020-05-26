eda6dc8635478102e34deff934b46e9095859e38
duc-nhat38 <phamdinhduc02071998@gmail.com>
2020-05-26 11:06:25 +0700

    haha

--- javascript/Object/car.js
+++ javascript/Object/car.js
@@ -2,9 +2,15 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 let img = document.getElementById("myCar");
-function Rectangle() {
-    ctx.fillStyle = "yellow"
-    ctx.fillRect(40, 40, 40, 40)
+function Rectangle(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.drawRect = function () {
+        ctx.fillStyle = "yellow"
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
 }
 function Racing(x, y, width, height) {
     this.x = x,
@@ -13,11 +19,36 @@ function Racing(x, y, width, height) {
         this.height = height
 
     this.myCar = function () {
-        
+
         ctx.drawImage(img, car.x, car.y, car.width, car.height);
     }
 }
-Rectangle();
+let draw1 = new Rectangle(40, 40, 40, 80);
+draw1.drawRect();
+let draw2 = new Rectangle(0, 160, 120, 80);
+draw2.drawRect();
+let draw3 = new Rectangle(120, 0, 80, 120);
+draw3.drawRect();
+let draw4 = new Rectangle(40, 280, 160, 40);
+draw4.drawRect();
+let draw5 = new Rectangle(160, 120, 40, 160);
+draw5.drawRect();
+let draw6 = new Rectangle(0, 360, 360, 40);
+draw6.drawRect();
+let draw7 = new Rectangle(240, 0, 200, 80);
+draw7.drawRect();
+let draw8 = new Rectangle(240, 120, 80, 160);
+draw8.drawRect();
+let draw9 = new Rectangle(240, 320, 120, 40);
+draw9.drawRect();
+let draw10 = new Rectangle(320, 120, 240, 40);
+draw10.drawRect();
+let draw11 = new Rectangle(360, 200, 80, 200);
+draw11.drawRect();
+let draw12 = new Rectangle(480, 40, 80, 320);
+draw12.drawRect();
+let draw13 = new Rectangle(520, 360, 40, 40);
+draw13.drawRect();
 let car = new Racing(0, 0, 40, 40)
 car.myCar();
 
@@ -32,6 +63,28 @@ function moveDown() {
 
 function moveRight() {
     car.x += 5;
+
+}
+function moveLeft() {
+    car.x -= 5;
+}
+function update() {
+    ctx.clearRect(0, 0, 600, 400);
+    car.myCar();
+    draw1.drawRect();
+    draw2.drawRect();
+    draw3.drawRect();
+    draw4.drawRect();
+    draw5.drawRect();
+    draw6.drawRect();
+    draw7.drawRect();
+    draw8.drawRect();
+    draw9.drawRect();
+    draw10.drawRect();
+    draw11.drawRect();
+    draw12.drawRect();
+    draw13.drawRect();
+    requestAnimationFrame(update);
 }
 
 function keyDown(e) {
@@ -39,10 +92,9 @@ function keyDown(e) {
     switch (e.keyCode) {
 
         case 39:
-            
+
             moveRight();
-            ctx.clearRect(0, 0, 600, 400);
-            car.myCar();
+
             break;
         case 38:
             moveUp();
@@ -50,8 +102,11 @@ function keyDown(e) {
         case 40:
             moveDown();
             break;
+        case 37:
+            moveLeft();
+            break;
     }
 }
 
 document.addEventListener("keydown", keyDown);
-
+update();
--- javascript/Object/mobile.js
+++ javascript/Object/mobile.js
@@ -1,46 +1,113 @@
-function Mobile1(){
-    
-    document.addEventListener('click', message);
-    document.addEventListener('click', betary);
-    document.addEventListener('click', turnOn);
-  
-}
+class Mobile {
+    constructor() {
+        this.bettary = 100;
+        this.memory1 = new Array();
+        this.memory2 = new Array();
+        this.memory3 = new Array();
+        this.status;
+        this.str;
+        this.ask;
+    }
+    turnOn() {
+        this.bettary -= 1;
+        this.status = true;
+    }
+    turnOff() {
+        this.bettary -= 1;
+        this.status = false;
+    }
+    checkStatus() {
+        if (this.status) {
+            this.bettary -= 1;
+            return this.status;
+        }
+    }
+    charge() {
+        if (this.status) {
+            if (this.bettary < 100) {
+                this.bettary += 1
+            }
+        }
+    }
+    texting() {
+        if (this.status) {
+            this.bettary -= 1;
+            this.str = prompt('Soạn tin nhắn :');
+            this.ask = confirm('Bạn có muốn gửi không');
 
-function turnOn(){
-    let ask1 = confirm('bạn muốn bật điện thoại không?');
-    if (ask1){
-        let ask2 = confirm('bạn muốn bật điện thoại nào?');
-        if(ask2) {
-            Mobile1()
-        }else {
-            document.write(iphone)
+        }
+    }
+
+
+    receive() {
+        if (this.status) {
+            this.bettary -= 1;
+            return this.memory3[this.memory3.length-1];
+
+
+        }
+    }
+
+    getSend() {
+        if (this.status) {
+        
+            this.bettary -= 1;
+            return this.memory2;
+        }
+    }
+    getReceive() {
+        if (this.status) {
+            this.bettary -= 1;
+
+            return this.memory3;
         }
     }
 }
 
-function message() {
-    let memory = [];
-    let write = prompt('Soạn tin nhắn : ');
-    let ask = confirm('Bạn muốn gửi tin nhắn');
-    if(ask){
-        confirm('tin nhắn :'+write);
-    }else {
-          memory.push(write);
+class Mobile1 extends Mobile {
+    constructor() {
+        super();
     }
-    
-}
-let pin = 100;
-function betary() {
-    
-    if (turnOn()){
-        pin-=100
-    }
-}
-function change(){
-    pin +=1;
-    if(pin===100){
-        break;
+    send(iphone) {
+        if (this.status) {
+            this.bettary -= 1;
+
+            if (this.ask) {
+       
+                this.memory2.push(this.str);
+                iphone.memory3.push(this.str);
+
+            } else {
+                this.memory1.push(this.str);
+            }
+        }
     }
 }
-turnOn()
-Mobile1();
+class Mobile2 extends Mobile {
+    constructor() {
+        super();
+    }
+    send(nokia) {
+        if (this.status) {
+            this.bettary -= 1;
+
+            if (this.ask) {
+                this.memory2.push(this.str);
+                nokia.memory3.push(this.str);
+            } else {
+                this.memory1.push(this.str);
+            }
+        }
+    }
+}
+let nokia = new Mobile1();
+nokia.turnOn();
+console.log(nokia.checkStatus());
+nokia.texting();
+let iphone = new Mobile2();
+iphone.turnOn();
+nokia.send(iphone);
+console.log(iphone.receive());
+console.log(iphone.getReceive());
+console.log(nokia.getSend());
+
